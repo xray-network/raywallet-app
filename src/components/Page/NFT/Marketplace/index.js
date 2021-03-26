4c9b7efcacc18691a8f095fd55dc8402da6d61aa
Ray Robot <hello@rraayy.com>
2021-03-26 21:32:58 +0200

    wip: nft section

--- src/components/Page/NFT/Marketplace/index.js
+++ src/components/Page/NFT/Marketplace/index.js
@@ -38,33 +38,6 @@ const NFTMarketplace = () => {
           </div>
         ))}
       </div>
-      <div className="ray__heading__nft">SpaceBudz Collection</div>
-      <div className={style.category}>
-        {_temp.spaceBudz.map((item, index) => (
-          <div key={index} className={style.itemOuter}>
-            <div className={style.item}>
-              <div className="d-flex mb-3">
-                <div className={style.itemCollection}>R</div>
-                <div className={style.itemCollection}>
-                  <i className="fe fe-user" />
-                </div>
-                <div className="ml-auto">
-                  <i className="fe fe-more-horizontal" />
-                </div>
-              </div>
-              <div className={style.itemImage}>
-                <img src={`https://spacebudz.io/spacebudz/bud${item}.png`} alt="" />
-              </div>
-              <div className={style.itemTitle}>
-                <strong>SpaceBud #{item}</strong>
-              </div>
-              <Tooltip title="Soon" placement="right">
-                <a className={`${style.itemLink} ray__link`}>Place Bid</a>
-              </Tooltip>
-            </div>
-          </div>
-        ))}
-      </div>
     </div>
   )
 }
